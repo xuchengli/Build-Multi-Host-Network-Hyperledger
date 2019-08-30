8af41e412b45672b6d400b2c5f1b31ccf239a8e4
Li Xu Cheng <lixucheng@ziggurat.cn>
2019-08-30 11:36:08 +0800

    add fabric orderer statup script

--- script-generate.sh
+++ script-generate.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 function fabric_ca() {
-  # sed on MacOSX does not support -i flag with a null extension. We will use
-  # 't' for our back-up's extension and delete it at the end of the function
-  ARCH=$(uname -s | grep Darwin)
-  if [ "$ARCH" == "Darwin" ]; then
-    OPTS="-it"
-  else
-    OPTS="-i"
-  fi
-
   cp ./templates/template_fabric_ca.sh fabric_ca.sh
   chmod +x fabric_ca.sh
 
@@ -27,6 +18,27 @@ function fabric_ca() {
   fi
 }
 
+function fabric_orderer() {
+  cp ./templates/template_fabric_orderer.sh fabric_orderer.sh
+  chmod +x fabric_orderer.sh
+
+  sed $OPTS "s/OVERLAY_NETWORK/${OVERLAY_NETWORK}/g" fabric_orderer.sh
+
+  # If MacOSX, remove the temporary backup of the docker-compose file
+  if [ "$ARCH" == "Darwin" ]; then
+    rm fabric_orderer.sht
+  fi
+}
+
+# sed on MacOSX does not support -i flag with a null extension. We will use
+# 't' for our back-up's extension and delete it at the end of the function
+ARCH=$(uname -s | grep Darwin)
+if [ "$ARCH" == "Darwin" ]; then
+  OPTS="-it"
+else
+  OPTS="-i"
+fi
 OVERLAY_NETWORK="fabric-net"
 
 fabric_ca
+fabric_orderer
---
+++ templates/template_fabric_orderer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker run --rm -it \
+           --network="OVERLAY_NETWORK" \
+           --name orderer.example.com \
+           -p 7050:7050 \
+           -e FABRIC_LOGGING_SPEC=INFO \
+           -e ORDERER_GENERAL_LISTENADDRESS=0.0.0.0 \
+           -e ORDERER_GENERAL_GENESISMETHOD=file \
+           -e ORDERER_GENERAL_GENESISFILE=/var/hyperledger/orderer/orderer.genesis.block \
+           -e ORDERER_GENERAL_LOCALMSPID=OrdererMSP \
+           -e ORDERER_GENERAL_LOCALMSPDIR=/var/hyperledger/orderer/msp \
+           -e ORDERER_GENERAL_TLS_ENABLED=true \
+           -e ORDERER_GENERAL_TLS_PRIVATEKEY=/var/hyperledger/orderer/tls/server.key \
+           -e ORDERER_GENERAL_TLS_CERTIFICATE=/var/hyperledger/orderer/tls/server.crt \
+           -e ORDERER_GENERAL_TLS_ROOTCAS=[/var/hyperledger/orderer/tls/ca.crt] \
+           -e ORDERER_KAFKA_TOPIC_REPLICATIONFACTOR=1 \
+           -e ORDERER_KAFKA_VERBOSE=true \
+           -e ORDERER_GENERAL_CLUSTER_CLIENTCERTIFICATE=/var/hyperledger/orderer/tls/server.crt \
+           -e ORDERER_GENERAL_CLUSTER_CLIENTPRIVATEKEY=/var/hyperledger/orderer/tls/server.key \
+           -e ORDERER_GENERAL_CLUSTER_ROOTCAS=[/var/hyperledger/orderer/tls/ca.crt] \
+           -e CORE_VM_DOCKER_HOSTCONFIG_NETWORKMODE=OVERLAY_NETWORK \
+           -v $(pwd)/channel-artifacts/genesis.block:/var/hyperledger/orderer/orderer.genesis.block \
+           -v $(pwd)/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp:/var/hyperledger/orderer/msp \
+           -v $(pwd)/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/:/var/hyperledger/orderer/tls \
+           -v $(pwd)/orderer.example.com:/var/hyperledger/production/orderer \
+           -w /opt/gopath/src/github.com/hyperledger/fabric \
+           hyperledger/fabric-orderer orderer
