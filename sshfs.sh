223fd3c6a79db40bed94b2d285f115a39e36fa8e
Li Xu Cheng <lixucheng@aliyun.com>
2019-10-16 13:51:45 +0800

    fix sshfs volume creation shell

--- sshfs.sh
+++ sshfs.sh
@@ -2,4 +2,7 @@
 
 docker plugin install --grant-all-permissions vieux/sshfs
 
-docker volume create --driver vieux/sshfs sshvolume
+docker volume create --driver vieux/sshfs \
+  -o sshcmd=root@152.32.170.29:/root/fabric-artifacts \
+  -o password=!Baas@test \
+  sshvolume
