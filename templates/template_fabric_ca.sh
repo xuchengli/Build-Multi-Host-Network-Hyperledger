64f7cb25ad75d191a26d3f15c70c799397ddfdde
Li Xu Cheng <lixucheng@aliyun.com>
2019-10-16 14:08:30 +0800

    add sshfs volume for fabric ca shell

--- templates/template_fabric_ca.sh
+++ templates/template_fabric_ca.sh
@@ -12,5 +12,6 @@ docker run --rm -it \
            -e FABRIC_CA_SERVER_CA_CERTFILE=/etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem \
            -e FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/CA_PRIVATE_KEY \
            -e CORE_VM_DOCKER_HOSTCONFIG_NETWORKMODE=OVERLAY_NETWORK \
-           -v $(pwd)/crypto-config/peerOrganizations/org1.example.com/ca/:/etc/hyperledger/fabric-ca-server-config \
+           --volume-driver vieux/sshfs \
+           --mount src=sshvolume,target=/etc/hyperledger/fabric-ca-server-config,volume-opt=sshcmd=root@152.32.170.29:/root/fabric-artifacts/crypto-config/peerOrganizations/org1.example.com/ca,volume-opt=password=!Baas@test \
            hyperledger/fabric-ca sh -c 'fabric-ca-server start -b admin:adminpw -d'
